12b37c4eac3010eb1e6c75405a3f92872bea5b72
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-04-15 10:08:24 +0530

    Create Settings.py

---
+++ Settings.py
@@ -0,0 +1,31 @@
+SET_BOT_MENU = False
+
+CMD_PREFIXES = "/"
+
+ADDONS = [
+    "start",
+    "help",
+    "about",
+    "id"
+]
+
+START_IN_GROUPS = "Thank you for starting me in your group. PM for questions!"
+
+
+ABOUT = "About This Bot \nDeveloped using @PyStark"
+
+HELP = """
+Available Commands
+
+{commands}
+/start - Start the bot
+/help - Show this message
+/about - About the bot
+/id - Get Telegram ID
+"""
+
+TIMEZONE = "Asia/Kolkata"
+
+DATABASE_TABLES = ["users", "bans"]
+
+MUST_JOIN = []
